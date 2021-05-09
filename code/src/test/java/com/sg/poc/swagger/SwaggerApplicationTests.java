0fa10664200d9849dd794b9f60054f0f202c87f8
Sandip <contact.mail.sandip@gmail.com>
2021-05-09 17:23:01 +0530

    initial commit

---
+++ code/src/test/java/com/sg/poc/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.poc.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
